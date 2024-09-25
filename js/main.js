ae398bc75477e2d76a8bc43028e83de067a769c8
IlyasSam <shambazovilyas@gmail.com>
2024-09-25 18:08:48 +0300

    Pr1

---
+++ js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+alert("Я JavaScript");
+let surname = prompt("Введите фамилию?");
+let name = prompt("Введите имя");
+let otchestvo = prompt("Введите отчество");
+alert(`Ваша фамилия: ${surname}, Ваше имя: ${name}, Ваше отчество: ${otchestvo}`);
+let isTrue = confirm(`Верны ли утверждения? Ваша фамилия: ${surname}, Ваше имя: ${name}, Ваше отчество: ${otchestvo}`);
